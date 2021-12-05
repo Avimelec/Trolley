6b4243982097df2e799e1cb5621690c9f0387b85
Avimelec <avimelecespinosa6@gmail.com>
2021-12-05 06:42:47 -0600

    carrito

---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./css/Banner.css";
+
+const Banner = () => {
+  return <div className="banner"></div>;
+};
+
+export default Banner;
+
---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: 'Inicio', 
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Nosotros', 
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contacto', 
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Mercado', 
+        url: '#',
+        cName: 'nav-links'
+    },
+]
+
